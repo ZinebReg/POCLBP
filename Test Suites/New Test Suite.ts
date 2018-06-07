b82022e6ca8e820616d829b964795b357fb1565d
zregragu <zregragu@FRPARESTCPOCLBP.corp.capgemini.com>
2018-06-07 10:01:01 +0200

    teeeest

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ec8d2496-56e2-42c7-accf-a877aefd7cc9</testSuiteGuid>
+   <testCaseLink>
+      <guid>2052c003-7440-4356-a235-c8da7522f202</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Affichage magasin/Main</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
